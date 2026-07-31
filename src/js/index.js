8070211879a02f817eb345706ae52b96d6bb5c6b
Filip Litvin <litvinfilip5@gmail.com>
2026-07-31 18:22:29 +0200

    noVideo

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+// import Notify from "simple-notify";
+// import "simple-notify/dist/simple-notify.css";
+
+// const footerBtn = document.querySelector("[data-btn]");
+
+// const myNotice = () => {
+//   new Notify({
+//     status: "succes",
+//     title: "Thanks for use this web-site!",
+//     text: "Created by Filip Litvin",
+//     effect: "side",
+//     speed: 500,
+//     customClass: null,
+//     customIcon: null,
+//     showIcon: true,
+//     showCloseButton: false,
+//     autoclose: true,
+//     autotimeout: 5000,
+//     gap: 20,
+//     distance: 20,
+//     type: "outline",
+//     position: "right top",
+//   });
+// };
+
+// const handkeClick = () => {
+//   myNotice();
+// };
+
+// footerBtn.addEventListener("click", handkeClick);
